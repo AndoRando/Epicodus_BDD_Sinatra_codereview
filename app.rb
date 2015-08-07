f1c5e9b861e6e9c2e5f8c69bd8168460436fb86c
Andrew Brennwald <leopawnz@gmail.com>
2015-08-07 11:43:55 -0700

    Update app.rb and create basic structure for homepage.

---
+++ app.rb
@@ -0,0 +1,12 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word_count')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:home)
+end
+
+get('/results') do
+  erb(:results)
+end
