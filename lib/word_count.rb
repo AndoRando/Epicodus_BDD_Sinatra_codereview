b3ac990183394aab39938a181b4abeb76c9e101a
Andrew Brennwald <leopawnz@gmail.com>
2015-08-07 10:40:17 -0700

    Add spec and passing code for splitting string into array using split() with regex arguments.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,5 +1,5 @@
 class String
   define_method(:word_count) do
-    string_to_be_searched = downcase()
+    string_to_be_searched = downcase().split(/\W*\s/)
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,7 +2,12 @@ require('rspec')
 require('word_count')
 
 describe("String#word_count") do
-  it("Downcases input string") do
-    expect("chuck".word_count()).to(eq("chuck"))
+
+  #it("Downcases the string") do
+  #  expect("chuck".word_count()).to(eq("chuck"))
+  #end
+
+  it("Splits the string into an array using regex to remove spaces and non-word characters") do
+    expect("What would, a Woodchuck chuck".word_count()).to(eq(["what", "would", "a", "woodchuck", "chuck"]))
   end
 end
